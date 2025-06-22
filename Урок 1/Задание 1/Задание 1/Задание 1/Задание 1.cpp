59dabe9707779cfab7e51037e1c597592163c6ba
Himitsu <aleksey15.03@mail.ru>
2025-06-22 20:37:53 +0300

    Добавьте файлы проекта.

---
+++ Урок 1/Задание 1/Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+enum Month {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+string getMonth(int monthNumber) {
+    switch (monthNumber) {
+    case January:   return "Январь";
+    case February:  return "Февраль";
+    case March:     return "Март";
+    case April:     return "Апрель";
+    case May:       return "Май";
+    case June:      return "Июнь";
+    case July:      return "Июль";
+    case August:    return "Август";
+    case September: return "Сентябрь";
+    case October:  return "Октябрь";
+    case November:  return "Ноябрь";
+    case December:  return "Декабрь";
+    default:        return "";
+    }
+}
+
+int main() {
+    setlocale(0, "rus");
+    int monthNumber;
+
+    while (true) {
+        cout << "Введите номер месяца: ";
+        cin >> monthNumber;
+
+        if (monthNumber == 0) {
+            cout << "До свидания" << endl;
+            break;
+        }
+
+        string monthName = getMonth(monthNumber);
+        if (!monthName.empty()) {
+            cout << monthName << endl;
+        }
+        else {
+            cout << "Неправильный номер!" << endl;
+        }
+    }
+
+    return 0;
+}
