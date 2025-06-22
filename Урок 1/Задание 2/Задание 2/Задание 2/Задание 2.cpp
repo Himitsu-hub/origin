93ad7ff873e1d8206af597fd6ec2354dfc1484fd
Himitsu <aleksey15.03@mail.ru>
2025-06-22 20:59:36 +0300

    2

---
+++ Урок 1/Задание 2/Задание 2/Задание 2/Задание 2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+struct BankAccount {
+    int accountNumber;     
+    string Name;      
+    double balance;        
+};
+
+void updateBalance(BankAccount& account, double newBalance) {
+    account.balance = newBalance;
+}
+
+int main() {
+    setlocale(0, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    BankAccount account;  
+
+    cout << "Введите номер счёта: ";
+    cin >> account.accountNumber;
+
+    cout << "Введите имя владельца: ";
+    cin.ignore();  
+    getline(cin, account.Name);
+
+    cout << "Введите баланс: ";
+    cin >> account.balance;
+
+    double newBalance;
+    cout << "Введите новый баланс: ";
+    cin >> newBalance;
+
+    updateBalance(account, newBalance);
+
+    cout << "Ваш счёт: " << account.Name << ", "
+        << account.accountNumber << ", " << account.balance << endl;
+
+  
+}
