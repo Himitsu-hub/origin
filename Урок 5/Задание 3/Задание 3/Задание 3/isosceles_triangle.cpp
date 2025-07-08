1d6f93f090663c7fce37e6ffda9c60b37058b8ab
Алексей Макаров <aleksey15.03@mail.ru>
2025-07-08 11:51:03 +0300

    Update isosceles_triangle.cpp

--- Урок 5/Задание 3/Задание 3/Задание 3/isosceles_triangle.cpp
+++ Урок 5/Задание 3/Задание 3/Задание 3/isosceles_triangle.cpp
@@ -1,5 +1,4 @@
 ﻿#include "isosceles_triangle.h"
-using namespace std;
 
 IsoscelesTriangle::IsoscelesTriangle(int a, int b, int A, int B)
     : Triangle(a, b, a, A, B, A, "Равнобедренный треугольник") {
