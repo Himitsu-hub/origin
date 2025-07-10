dc72ef484e0f505fa41a7d5a6df62bcfa22ec311
Himitsu <aleksey15.03@mail.ru>
2025-07-10 14:44:24 +0300

    2

---
+++ Урок 10/Задание 2/Leaver/LeaverClient/LeaverClient.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include "Leaver.h"
+
+int main() {
+    setlocale(0, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Leaver leaver;
+    std::string name;
+
+    std::cout << "Введите имя: ";
+    std::getline(std::cin, name);
+
+    std::cout << leaver.leave(name) << std::endl;
+
+    return 0;
+}
