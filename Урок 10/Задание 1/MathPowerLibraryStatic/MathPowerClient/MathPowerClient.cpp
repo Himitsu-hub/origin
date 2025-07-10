2fd663ab02f842c259420ea4fc7d5ef6ec51e630
Himitsu <aleksey15.03@mail.ru>
2025-07-10 14:04:56 +0300

    1

---
+++ Урок 10/Задание 1/MathPowerLibraryStatic/MathPowerClient/MathPowerClient.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include "Greeter.h"
+
+int main() {
+    setlocale(0, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Greeter greeter;
+    std::string name;
+
+    std::cout << "Введите имя: ";
+    std::getline(std::cin, name);
+
+    std::cout << greeter.greet(name) << std::endl;
+
+    return 0;
+}
---
+++ Урок 10/Задание 1/MathPowerLibraryStatic/MathPowerLibraryStatic/Greeter.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <string>
+
+class Greeter {
+public:
+    std::string greet(const std::string& name);
+};
+
