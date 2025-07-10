e68996ad7a68f2345413baf3f77138b2c4cde46e
Himitsu <aleksey15.03@mail.ru>
2025-07-10 15:36:33 +0300

    3

---
+++ Урок 10/Задание 3/LeaverLib/LeaverLib/Leaver.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#ifndef LEAVER_H
+#define LEAVER_H
+
+#ifdef LEAVER_EXPORTS
+#define LEAVER_API __declspec(dllexport)
+#else
+#define LEAVER_API __declspec(dllimport)
+#endif
+
+#include <string>
+
+class LEAVER_API Leaver {
+public:
+    std::string leave(const std::string& name);
+};
+
+#endif
